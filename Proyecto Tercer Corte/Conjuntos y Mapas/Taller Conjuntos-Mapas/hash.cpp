12a62e21f237d1969cd9738f5f33fc2ebb6b2b75
CvrlosLucero <carlosj.lucero@urosario.edu.co>
2024-05-06 22:11:40 -0500

    Update hash.cpp

--- Proyecto Tercer Corte/Conjuntos y Mapas/Taller Conjuntos-Mapas/hash.cpp
+++ Proyecto Tercer Corte/Conjuntos y Mapas/Taller Conjuntos-Mapas/hash.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <string>
+#include <map>
 
 using namespace std;
 
+pair<string, int> simpleHash(string& texto){
+    int hash = 0;
+    for(char c : texto){
+        hash += c;
+    }
+    return make_pair(texto, hash);
+}
+
 int main() {
-    
-    
+    pair<string, int> hashes;
+
+    cout<<"Ingrese el texto a hashear:"<<endl;
+    cin.ignore();
+    getline(cin, hashes.first);
+
+    hashes = simpleHash(hashes.first);
+
+    cout<<"Texto: "<<hashes.first<<endl;
+    cout<<"Hash: "<<hashes.second<<endl;
 
     return 0;
 }
